d22167126232e5c9433ac0f253e73c757fdaf789
Константин Дьяченко <konard@me.com>
2019-10-08 20:57:09 +0300

    Codacy issues fix in push-nuget.sh.

--- push-nuget.sh
+++ push-nuget.sh
@@ -13,22 +13,22 @@ dotnet pack -c Release
 # Get version string
 PackageFileNamePrefix="Platform.${TRAVIS_REPO_NAME}/bin/Release/Platform.${TRAVIS_REPO_NAME}."
 PackageFileNameSuffix=".nupkg"
-PackageFileName=$(echo ${PackageFileNamePrefix}*${PackageFileNameSuffix})
+PackageFileName=$(echo "${PackageFileNamePrefix}"*"${PackageFileNameSuffix}")
 Version="${PackageFileName#$PackageFileNamePrefix}"
 Version="${Version%$PackageFileNameSuffix}"
 
 # Ensure NuGet package does not exist
 NuGetPackageUrl="https://globalcdn.nuget.org/packages/Platform.${TRAVIS_REPO_NAME}.${Version}${PackageFileNameSuffix}"
 NuGetPackageUrl=$(echo "$NuGetPackageUrl" | tr '[:upper:]' '[:lower:]')
-NuGetPageStatus="$(curl -Is ${NuGetPackageUrl} | head -1)"
+NuGetPageStatus="$(curl -Is "${NuGetPackageUrl}" | head -1)"
 StatusContents=( $NuGetPageStatus )
-if [ ${StatusContents[1]} == "200" ]; then
+if [ "${StatusContents[1]}" == "200" ]; then
   echo "NuGet with current version is already pushed."
   exit 0
 fi
 
 # Push NuGet package
-dotnet nuget push ./**/*.nupkg -s https://api.nuget.org/v3/index.json -k ${NUGETTOKEN} 
+dotnet nuget push ./**/*.nupkg -s https://api.nuget.org/v3/index.json -k "${NUGETTOKEN}" 
 
 # Clean up
 find . -type f -name '*.nupkg' -delete
