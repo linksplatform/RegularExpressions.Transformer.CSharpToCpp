9d21d70d8234b99e2a8d3bde48c4e76ec6963c13
Константин Дьяченко <konard@me.com>
2019-10-18 14:50:58 +0300

    Using $CURRENT_BRANCH instead of $GITHUB_REF

--- publish-release.sh
+++ publish-release.sh
@@ -2,11 +2,11 @@
 set -e # Exit with nonzero exit code if anything fails
 
 echo $GITHUB_EVENT_NAME
-echo $GITHUB_REF
+echo $CURRENT_BRANCH
 echo $DEFAULT_BRANCH
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [[ ( "$GITHUB_EVENT_NAME" != "push" ) || ( "$GITHUB_REF" != "$DEFAULT_BRANCH" ) ]]; then
+if [[ ( "$GITHUB_EVENT_NAME" != "push" ) || ( "$CURRENT_BRANCH" != "$DEFAULT_BRANCH" ) ]]; then
     echo "Skipping release deploy."
     exit 0
 fi
